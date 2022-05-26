87422cf8884ed98cf022257ecd986bb62e24606b
sauritl <57330547+sauritl@users.noreply.github.com>
2022-05-26 15:28:09 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -35,6 +35,6 @@ spotify_csv = st.secrets["spotify_csv"]
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{spotify_csv}"')
 spotify_csv = pd.DataFrame(rows)
-to_show = spotify_csv['user_name']== str(select)
+to_show = spotify_csv['user_name'] equal str(select)
 st.write(to_show)
 
